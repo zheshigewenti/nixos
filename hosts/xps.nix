835e94ea6b5ecec6eaa64cdc81e3c69263eee1cf
vincent <dzn1534564656@gmail.com>
2026-09-13 03:56:55 +0800

    update

--- hosts/xps.nix
+++ hosts/xps.nix
@@ -6,8 +6,6 @@
   ...
 }: {
   imports = [
-    ../modules/common.nix
-    ../modules/nixvim.nix
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
@@ -34,13 +32,6 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # XPS 专属配置
-  boot.extraModprobeConfig = ''
-    blacklist i8k
-    blacklist dell_wmi_ddv
-    blacklist dell_smm_hwmon
-    blacklist dell_smm
-  '';
   boot.blacklistedKernelModules = [
     "dell_wmi_ddv"
     "i8k"
